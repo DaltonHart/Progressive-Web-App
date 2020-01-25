c19ec33d9bf739fea012ec409e15063fff87388f
daltonhart <daltonvaren@gmail.com>
2020-01-25 15:53:13 -0800

    adds final sw

--- public/sw.js
+++ public/sw.js
@@ -4,8 +4,8 @@ importScripts('/src/js/idb.js');
 // improt utility for all db functionality
 importScripts('/src/js/utility.js');
 
-const CACHE_STATIC_NAME = 'static-v25';
-const CACHE_DYNAMIC_NAME = 'dynamic-v6';
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHED_STATIC_URLS = [
   '/',
   '/index.html',
